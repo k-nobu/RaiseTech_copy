42e2cad91861d6496c2f739d10953f016a878fdb
nobukazu kodama <pgm.nobu@gmail.com>
2021-11-24 04:05:45 +0900

    メニュー

---
+++ js/jquery.js
@@ -0,0 +1,59 @@
+$(function(){
+
+  $('.open-menu1').hover(
+  function(){
+    $('.sub-menu1').slideDown(230);
+    },
+    function(){
+      $('.sub-menu1').hide();
+    }
+  );
+});
+
+$(function(){
+
+  $('.open-menu2').hover(
+  function(){
+    $('.sub-menu2').slideDown(230);
+    },
+    function(){
+      $('.sub-menu2').hide();
+    }
+  );
+});
+
+$(function(){
+
+  $('.open-menu3').hover(
+  function(){
+    $('.sub-menu3').slideDown(230);
+    },
+    function(){
+      $('.sub-menu3').hide();
+    }
+  );
+});
+
+$(function(){
+
+  $('.open-menu4').hover(
+  function(){
+    $('.sub-menu4').slideDown(230);
+    },
+    function(){
+      $('.sub-menu4').hide();
+    }
+  );
+});
+
+$(function(){
+
+  $('.open-menu5').hover(
+  function(){
+    $('.sub-menu5').slideDown(230);
+    },
+    function(){
+      $('.sub-menu5').hide();
+    }
+  );
+});
