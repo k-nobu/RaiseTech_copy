19549cbc85bdd4290db49a4f6245b5f092f44126
nobukazu kodama <pgm.nobu@gmail.com>
2021-11-30 05:11:18 +0900

    ハンバーガーメニュー×まで

--- js/jquery.js
+++ js/jquery.js
@@ -57,3 +57,16 @@ $(function(){
     }
   );
 });
+
+/*ハンバーガーメニュー*/
+$(function() {
+  $('.hamburger').click(function() {
+      $(this).toggleClass('active');
+
+      if ($(this).hasClass('active')) {
+          $('.globalMenuSp').addClass('active');
+      } else {
+          $('.globalMenuSp').removeClass('active');
+      }
+  });
+});
